6f6d8f3c4adbc27eda49456d8dad4654ad875c25
Ben Hanover <benhanover4@gmail.com>
2021-01-29 10:57:10 +0200

    added count
    added section for sort and count
    added sort button
    added count functinality

--- src/main.js
+++ src/main.js
@@ -2,14 +2,22 @@
 const input = document.querySelector('#text-input');
 const addButton = document.querySelector('#add-button');
 const list = document.querySelector('.view-section');
-const priority = document.querySelector('#priority-selector')
+const priority = document.querySelector('#priority-selector');
+const countAndSortSection = document.querySelector('.countAndSortSection');
+addCountAndSort();
+
 
 // Event Listeners
 addButton.addEventListener('click', addTask);
+ 
 list.addEventListener('click', deleteAndCheck);
+
 
 // Functions
 function addTask(e) {
+  // increase the counter
+  addButton.value++;
+  document.querySelector('.counter').innerText = `Tasks so far - ${addButton.value}`;
   // creating the div with class todo-container
   const containerDiv = document.createElement('div');
   containerDiv.classList.add("todo-container");
@@ -57,15 +65,29 @@ function addTask(e) {
   // reset the input value
   input.value = "";
 }
-
+//  delete and check function 
 function deleteAndCheck(e) {
   const target = e.target;
   item = target.parentElement;
   if (target.className === 'trash-btn') {
     item.remove();
+    addButton.value--;
+    document.querySelector('.counter').innerText = `Tasks so far - ${addButton.value}`;
   }
   if (target.className === 'complete-btn') {
     item.classList.toggle('completed');
   } 
 }
-
+// creates the count and sort
+function addCountAndSort() {
+  // creating the counter
+  const counter = document.createElement('h2');
+  counter.classList.add('counter');
+  counter.innerText = `Tasks so far - 0`;
+  countAndSortSection.appendChild(counter);
+  // creating the sort button
+  const sortButton = document.createElement('button');
+  sortButton.classList.add("sort-btn");
+  sortButton.innerText = 'Sort by priority';
+  countAndSortSection.appendChild(sortButton);
+}
