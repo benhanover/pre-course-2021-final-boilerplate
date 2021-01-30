96741cde90e519d5209f10d347322ab8257a4c24
Ben Hanover <benhanover4@gmail.com>
2021-01-30 15:07:08 +0200

    REPLACED LOCAL STORAGE WITH JSON BIN

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ sort.addEventListener('click', sortByPriority);
 
 
 // Functions
-function addTask(e) {
+function addTask(task) {
   // increase the counter
   addButton.value++;
   document.querySelector('.counter').innerText = `Tasks so far - ${addButton.value}`;
@@ -25,30 +25,51 @@ function addTask(e) {
   // append it to the list
   list.appendChild(containerDiv);
 
-  // creating the div with class todo-priority
+  // creating the div with class todo-priority************
   const priorityDiv = document.createElement('div');
   priorityDiv.classList.add("todo-priority");
   priorityDiv.setAttribute("value", priority.value);
-  priorityDiv.innerText = priority.value;
+  if (task.priority !== undefined) {
+    priorityDiv.innerText = task.priority;
+  }
+  // came from the client
+  else {
+    priorityDiv.innerText = priority.value;
+  }
   // appending to the container
   containerDiv.appendChild(priorityDiv);
 
-  // creating the div with class todo-created-at
+
+  // creating the div with class todo-created-at**************
   const dateDiv = document.createElement('div');
   dateDiv.classList.add("todo-created-at");
-  const date = new Date();
-  const years = date.toISOString().split('T')[0];
-  const month = date.toISOString().split('T')[1].split('.')[0];
-  const currentTime = `${years}
-   ${month}`;
+  let date;
+  // came from the api
+  let currentTime;
+  if (task.date !== undefined) {
+    date = new Date(task.date);
+    currentTime = changeDateFormat(date);
+  }
+  // came from the client
+  else {
+    date = new Date();
+    currentTime = changeDateFormat(date); 
+  }
   dateDiv.innerText = currentTime; 
    // appending to the container
   containerDiv.appendChild(dateDiv);
 
-  // creating the div with class todo-text
+  // creating the div with class todo-text**********
   const textDiv = document.createElement('div');
   textDiv.classList.add("todo-text");
-  textDiv.innerText = input.value;
+  // came from the api
+  if (task.text !== undefined) {
+    textDiv.innerText = task.text;
+  }
+  // came from the client
+  else {
+    textDiv.innerText = input.value;
+  }
    // appending to the container
   containerDiv.appendChild(textDiv);
 
@@ -63,6 +84,9 @@ function addTask(e) {
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
   trashButton.classList.add('trash-btn');
   containerDiv.appendChild(trashButton);
+
+  // update the api
+  updateBin();
 
   // reset the input value
   input.value = "";
@@ -81,6 +105,16 @@ function deleteAndCheck(e) {
     item.classList.toggle('completed');
   } 
 }
+
+// changes the date object to sql format
+function changeDateFormat(date) {
+  const years = date.toISOString().split('T')[0];
+  const month = date.toISOString().split('T')[1].split('.')[0];
+  const currentTime = `${years}
+   ${month}`;
+  return currentTime;
+}
+
 // creates the count and sort
 function addCountAndSort() {
   // creating the counter
@@ -106,3 +140,49 @@ function sortByPriority() {
     }
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////////
+// API
+  // update
+const updateBin = async () => {
+  const UPDATE_BIN_URL = `https://api.jsonbin.io/v3/b/60154a5daafcad2f59618c13`;
+  let todoList = [];
+  const allDates = document.querySelectorAll(
+    "body > div.view-section-wrapper > div > div > div.todo-created-at"
+  );
+  const allTextInputs = document.querySelectorAll(
+    "body > div.view-section-wrapper > div > div > div.todo-text"
+  );
+  const allPriorities = document.querySelectorAll(
+    "body > div.view-section-wrapper > div > div > div.todo-priority"
+  );
+  for (let j = 0; j < allDates.length; j++) {
+    const obj = {
+      date: allDates[j].textContent,
+      text: allTextInputs[j].textContent,
+      priority: allPriorities[j].textContent,
+    };
+    todoList.push(obj);
+  }
+  const binUpdate = await fetch(UPDATE_BIN_URL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ "my-todo": todoList }),
+  });
+};
+
+// read
+const readBin = async () => {
+  URL = "https://api.jsonbin.io/v3/b/60154a5daafcad2f59618c13/latest";
+  const serverResponse = await fetch(URL);
+  const wantedResponse = await serverResponse.json();
+  const todoList = await wantedResponse.record["my-todo"];
+  for (const todo of todoList) {
+    addTask(todo);
+  }
+}
+
+readBin();
+
