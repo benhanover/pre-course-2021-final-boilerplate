d27361534d72c4da7dcca5138442afa1923319eb
Ben Hanover <benhanover4@gmail.com>
2021-01-30 13:46:32 +0200

    Revert "local storage"
    This reverts commit 5fb4fbd71b1cb728250159e8aa9f454547519886.

--- src/main.js
+++ src/main.js
@@ -4,27 +4,17 @@ const addButton = document.querySelector('#add-button');
 const list = document.querySelector('.view-section');
 const priority = document.querySelector('#priority-selector');
 const countAndSortSection = document.querySelector('.countAndSortSection');
-// adds them to the html
 addCountAndSort();
 const sort = document.querySelector('.sort-button');
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-
 // Event Listeners
-document.addEventListener('DOMContentLoaded', getTasks);
 addButton.addEventListener('click', addTask);
 list.addEventListener('click', deleteAndCheck);
 sort.addEventListener('click', sortByPriority);
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-
 // Functions
-  // ADD TASK FUNCTION-
-    // - increasing the counter of tasks
-    // creating div container and assigns to it the 3 propetys
-    // adding check and delete button to every task
 function addTask(e) {
   // increase the counter
   addButton.value++;
@@ -38,8 +28,8 @@ function addTask(e) {
   // creating the div with class todo-priority
   const priorityDiv = document.createElement('div');
   priorityDiv.classList.add("todo-priority");
+  priorityDiv.setAttribute("value", priority.value);
   priorityDiv.innerText = priority.value;
-
   // appending to the container
   containerDiv.appendChild(priorityDiv);
 
@@ -47,8 +37,6 @@ function addTask(e) {
   const dateDiv = document.createElement('div');
   dateDiv.classList.add("todo-created-at");
   const date = new Date();
-
-  // costumizing the date to SQL FORMAT
   const years = date.toISOString().split('T')[0];
   const month = date.toISOString().split('T')[1].split('.')[0];
   const currentTime = `${years}
@@ -76,9 +64,6 @@ function addTask(e) {
   trashButton.classList.add('trash-btn');
   containerDiv.appendChild(trashButton);
 
-  // add task to localStorage
-  saveLocalTasks([priority.value, currentTime, input.value]);
-
   // reset the input value
   input.value = "";
 }
@@ -102,9 +87,7 @@ function addCountAndSort() {
   const counter = document.createElement('h2');
   counter.classList.add('counter');
   counter.setAttribute("id", "counter");
-
   counter.innerText = `Tasks so far - 0`;
-
   countAndSortSection.appendChild(counter);
   // creating the sort button
   const sortButton = document.createElement('button');
@@ -122,76 +105,4 @@ function sortByPriority() {
       }
     }
   }
-}
-// saves to local storage
-function saveLocalTasks(task) {
-  // check if i have things in the local storage
-  let todos;
-  if (localStorage.getItem('todos') === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem('todos'));
-  }
-  todos.push(task);
-  localStorage.setItem('todos', JSON.stringify(todos));
-}
-
-// get from the local storage by printing the array all over again 
-function getTasks() {
-  // check if i have things in the local storage
-  let todos;
-  if (localStorage.getItem('todos') === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem('todos'));
-  }
-  todos.forEach(function (todo) {
-    // creating the div with class todo-container
-    const containerDiv = document.createElement('div');
-    containerDiv.classList.add("todo-container");
-    // append it to the list
-    list.appendChild(containerDiv);
-
-    // creating the div with class todo-priority
-    const priorityDiv = document.createElement('div');
-    priorityDiv.classList.add("todo-priority");
-    priorityDiv.innerText = todo[0];
-
-    // appending to the container
-    containerDiv.appendChild(priorityDiv);
-
-    // creating the div with class todo-created-at
-    const dateDiv = document.createElement('div');
-    dateDiv.classList.add("todo-created-at");
-    const date = new Date();
-
-    // costumizing the date to SQL FORMAT
-    const years = date.toISOString().split('T')[0];
-    const month = date.toISOString().split('T')[1].split('.')[0];
-    const currentTime = `${years}
-   ${month}`;
-    dateDiv.innerText = todo[1];
-    // appending to the container
-    containerDiv.appendChild(dateDiv);
-
-    // creating the div with class todo-text
-    const textDiv = document.createElement('div');
-    textDiv.classList.add("todo-text");
-    textDiv.innerText = todo[2];
-    // appending to the container
-    containerDiv.appendChild(textDiv);
-
-    // check mark button
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add("complete-btn");
-    containerDiv.appendChild(completedButton);
-
-    // delete button
-    const trashButton = document.createElement('button');
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('trash-btn');
-    containerDiv.appendChild(trashButton);
-
-  });
 }
